ae65b93bced98e76596afaf042782773d1da9b9f
Julie Groth <groth.julie@gmail.com>
2018-04-22 18:08:18 -0700

    changed note model to track

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,26 +2,24 @@ const mongoose = require("mongoose");
 const db = require("../models");
 mongoose.Promise = global.Promise;
 
-// This file empties the Books collection and inserts the books below
+// This file empties the Notes collection and inserts the notes below
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/treatYoSelf"
 );
 
-const noteSeed = [
+const trackSeed = [
   {
-    title: "Test title 1",
-    note: 'test note 1',
+    other: 'test note 1',
    },
   {
-    title: "Test title 2",
-    note: "Test note 2",
+    other: 'test note 2'
   }
 ];
 
-db.Note
+db.Track
   .remove({})
-  .then(() => db.Note.collection.insertMany(noteSeed))
+  .then(() => db.Track.collection.insertMany(trackSeed))
   .then(data => {
     console.log(data.insertedIds.length + " records inserted!");
     process.exit(0);
