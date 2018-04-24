03195a26a799888e84011874411adc52364b752a
jbusta93 <jbusta93@gmail.com>
2018-04-23 20:19:00 -0700

    added jsonwebtoken dependency

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const passport = require('passport');
 
 // use body parser
 app.use(bodyParser.urlencoded({ extended: false }));
