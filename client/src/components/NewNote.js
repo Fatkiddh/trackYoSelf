e99905262ccd8d1672057f29c189cbc3051ece6e
Julie Groth <groth.julie@gmail.com>
2018-04-24 22:59:02 -0700

    added newnote route and updated newnote component

--- client/src/components/NewNote.js
+++ client/src/components/NewNote.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Tag } from 'react-materialize';
 
 const NewNote = () =>
   <form>
     <div className="row">
       <div className="col s12">
-        <input placeholder="Title" id="title"/>
-      </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <textarea id="body" className="materialize-textarea"></textarea>
-          <label for="textarea1">"How are you feeling today?"</label>
-        </div>
+        <input placeholder="Track Title" id="title" />
+        <input name='on' type='date' onChange={function (e, value) { }} />
       </div>
     </div>
+
+    <div className="row">
+      <div className="input-field col s12">
+        <textarea id="body" className="materialize-textarea"></textarea>
+        <label type="textarea">"How are you feeling today?"</label>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col s12">
+        <Tag>Tag</Tag>
+        <Tag>Tag 2</Tag>
+      </div>
+    </div>
+
   </form>;
 
 export default NewNote;
