45c21aa8fce39c43fc62f28ff7448452d6702b8b
Alvin Go <alvingogo25@gmail.com>
2018-04-25 22:31:26 -0700

    dashboard page and recents list

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Row, Col} from 'react-materialize';
+import RecentTracks from '../components/RecentTracks';
+
+// 2 chart boxes and recents list
+const Dashboard = () =>
+  <Row className="container">
+    <Col s={12} m={8}>
+      <Row>
+        Chart component of last month here.
+      </Row>
+      <Row>
+        Chart component of averages here.
+      </Row>
+    </Col>
+    <Col s={12} m={4}>
+      <RecentTracks />;
+    </Col>
+  </Row>
+export default Dashboard;
