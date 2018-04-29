20134f08ad99ed012d16904f8d80d25d38daabd8
Alvin Go <alvingogo25@gmail.com>
2018-04-29 15:27:17 -0700

    fixed component error

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Launch from './pages/Launch';
 import Dashboard from './pages/Dashboard';
-import SignUpForm from './components/SignUpForm';
-import LogInForm from './components/LogInForm';
+// import SignUpForm from './components/SignUpForm';
+// import LogInForm from './components/LogInForm';
 import NewTrack from './components/NewTrack';
 
 
@@ -12,10 +12,9 @@ const App = () =>
   <Router>
     <div>
       <Navbar />
-          <Route exact path="/" component={Launch} />
+          <Route path="/" component={Launch} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Route path="/signup" component={SignUpForm} />
-          <Route path="/login" component={LogInForm} />
+
           <Route exact path="/new" component={NewTrack} />
     </div>
   </Router>;
